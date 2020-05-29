6841b0f097ac3a8f68145e1fbc5639507e98099a
Your Name <you@example.com>
2020-05-29 12:36:40 -0600

    login logic from db for homepage

---
+++ ClientApp/src/components/Form.js
@@ -0,0 +1,78 @@
+﻿import React, { Component } from 'react'
+import { Button, Form, Segment } from 'semantic-ui-react'
+
+class FormExample extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            users: [],
+            username: " ",
+            password: " "
+        };
+    }
+
+    checkUsername = () => {
+            var name = this.state.users.map(user =>
+                user.username
+        ).indexOf(this.state.username);
+        return name;
+    }
+
+    checkPassword = (name) => {
+        var pass = this.state.users[name].password;
+        console.log(pass);
+        if (pass == this.state.password)
+            return true;
+        else
+            return false;
+    }
+
+    validate = () => {
+        var name = this.checkUsername();
+        if (name != -1) {
+            if (this.checkPassword(name))
+                console.log("LOG IN");
+            else
+                console.log("WRONG PASS");
+        }
+        else
+            console.log("NEW USER");
+    }
+
+    onChangeUser = (e) => {
+        this.setState({ username : e.target.value })
+    }
+
+    onChangePass = (e) => {
+        this.setState({ password: e.target.value })
+    }
+
+    componentDidMount() {
+        this.populateUserData();
+    }
+
+    render() {
+        return(
+            <Segment inverted>
+                <Form inverted>
+                    <Form.Group>
+                        <Form.Input fluid label='Username' placeholder='Username' value={this.state.username} width={15} onChange={this.onChangeUser} />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Input fluid label='Password' placeholder='Password' value={this.state.password} width={15} onChange={this.onChangePass} />
+                    </Form.Group>
+                    <Button type='submit' onClick={this.validate}>Submit</Button>
+                </Form>
+            </Segment>
+        )
+    }
+
+    async populateUserData() {
+        const response = await fetch('/api/Users');
+        const data = await response.json();
+        this.setState({ users: data });
+    }
+}
+export default FormExample
--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -7,22 +7,15 @@ export class Home extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { users: [], loading: true };
     }
 
-    componentDidMount() {
-        this.populateUserData();
-    }
+    
 
     render() {
         return (
-            <ModalExample />
+            <ModalExample/>
             )
     }
 
-    async populateUserData() {
-        const response = await fetch('/api/Users');
-        const data = await response.json();
-        this.setState({ users: data, loading: false });
-    }
+    
 }
